8077222dd898f7546d0eca5880884fd234219c64
Anton Melentiev <info@AntonMelentiev.com>
2022-01-12 14:05:25 +0100

    upd: header - move elements to properties;

--- page_elements/header.py
+++ page_elements/header.py
@@ -13,10 +13,50 @@ class Header:
     def __init__(self, driver: webdriver):
         self._driver = driver
 
+    ##############################
+    # Login dropdown
+    ##############################
+    @property
+    def _login_dropdown(self):
+        return self._driver.find_element_by_id("login_dropdown")
+
+    @property
+    def _username_input(self):
+        return self._driver.find_element_by_id("username_input")
+
+    @property
+    def _password_input(self):
+        return self._driver.find_element_by_id("password_input")
+
+    @property
+    def _submit_login_btn(self):
+        return self._driver.find_element_by_id("login_btn")
+
+    ##############################
+    # User dropdown
+    ##############################
+    @property
+    def _user_dropdown(self):
+        return self._driver.find_element_by_id("user_dropdown")
+
+    @property
+    def _user_page_lnk(self):
+        return self._driver.find_element_by_id("UNuser")
+
+    ##############################
+    # Main navigation
+    ##############################
+    @property
+    def _admin_page_nav_btn(self):
+        return self._driver.find_element_by_id("HBadmingames")
+
+    ####################################################################################################################
+    # Methods
+    ####################################################################################################################
+
     @allure.step
     def open_login_dropdown(self):
-        login_dropdown = self._driver.find_element_by_id("login_dropdown")
-        login_dropdown.click()
+        self._login_dropdown.click()
         WebDriverWait(driver=self._driver, timeout=10).until(
             expected_conditions.visibility_of_element_located((By.ID, "login_btn"))
         )
@@ -24,12 +64,9 @@ class Header:
     @allure.step
     def login(self, username: str, password: str, timeout: Union[float, int] = TIMEOUT):
         self.open_login_dropdown()
-        username_input = self._driver.find_element_by_id("username_input")
-        password_input = self._driver.find_element_by_id("password_input")
-        submit_login_btn = self._driver.find_element_by_id("login_btn")
-        username_input.send_keys(username)
-        password_input.send_keys(password)
-        submit_login_btn.click()
+        self._username_input.send_keys(username)
+        self._password_input.send_keys(password)
+        self._submit_login_btn.click()
 
         WebDriverWait(driver=self._driver, timeout=timeout).until(
             expected_conditions.visibility_of_element_located((By.ID, "user_dropdown")),
@@ -38,19 +75,16 @@ class Header:
 
     @allure.step
     def open_user_dropdown(self):
-        user_dropdown = self._driver.find_element_by_id("user_dropdown")
-        user_dropdown.click()
-        WebDriverWait(driver=self._driver, timeout=10).until(
+        self._user_dropdown.click()
+        WebDriverWait(driver=self._driver, timeout=2).until(
             expected_conditions.visibility_of_element_located((By.ID, "UNuser"))
         )
 
     @allure.step
     def open_user_page_from_dropdown(self):
         self.open_user_dropdown.click()
-        user_page_lnk = self._driver.find_element_by_id("UNuser")
-        user_page_lnk.click()
+        self._user_page_lnk.click()
 
     @allure.step
     def open_game_list(self):
-        admin_page = self._driver.find_element_by_id("HBadmingames")
-        admin_page.click()
+        self._admin_page_nav_btn.click()
